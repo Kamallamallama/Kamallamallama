acfba58146649c0e0291150d414895f81a269a80
Kamila <ktasybekova66@gmail.com>
2021-10-28 03:43:23 +0600

    First HW

---
+++ hw1.rb
@@ -0,0 +1,10 @@
+print "What is your name? "
+name = gets.chomp
+puts "Hi #{name} . What is your height? "
+height = gets.chomp.to_f
+ideal_weight = (height-110)*1.15
+if (ideal_weight < 0)
+print 'your weight is already optimal '
+else
+puts "your ideal weight is #{ideal_weight} "
+end
